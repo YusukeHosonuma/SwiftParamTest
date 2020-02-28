6a3e7e4c47f09087531c1bd1562f17ae53f11a2a
Yusuke Hosonuma <tobi462@gmail.com>
2020-02-28 17:14:49 +0900

    $ make format

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -1,20 +1,16 @@
 //
-//  ViewController.swift
-//  Example
+// ViewController.swift
+// SwiftParamTest
 //
-//  Created by Yusuke Hosonuma on 2020/02/26.
-//  Copyright © 2020 Yusuke Hosonuma. All rights reserved.
+// Created by Yusuke Hosonuma on 2020/02/26.
+// Copyright (c) 2020 Yusuke Hosonuma.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
