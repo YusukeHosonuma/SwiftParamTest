eec98b11b528ec4c9e7b1e78ec4ad04e9cd699e9
Yusuke Hosonuma <tobi462@gmail.com>
2020-03-13 11:44:25 +0900

    fix: @available annotation

--- Sources/DSL/FunctionBuilderDSL.swift
+++ Sources/DSL/FunctionBuilderDSL.swift
@@ -9,31 +9,6 @@
 // Note:
 // Function Builder is supported by Swift 5.1 or later.
 
-// MARK: - Operators
-
-precedencegroup ExpectRowPrecedence {
-    lowerThan: FunctionArrowPrecedence
-}
-
-infix operator ==>: ExpectRowPrecedence
-
-/// Create expectation row.
-/// - Parameters:
-///   - lhs: parameters of test target function
-///   - rhs: expected value
-@available(swift 5.1)
-public func ==> <T, R>(lhs: T, rhs: R) -> ExpectRow<T, R> {
-    ExpectRow(args: lhs, expect: rhs)
-}
-
-@available(swift 5.1)
-public struct ExpectRow<T, R> {
-    var args: T
-    var expect: R
-}
-
-// MARK: - Function Builders
-
 @available(swift 5.1)
 @_functionBuilder
 public struct ParameterBuilder<T, R> {
@@ -41,8 +16,6 @@ public struct ParameterBuilder<T, R> {
         rows
     }
 }
-
-// MARK: - DSL
 
 /// Create `Expect` for start parameterized-test.
 /// - Parameters:
@@ -55,13 +28,4 @@ public func assert<T, R: Equatable>(
     @ParameterBuilder <T, R> rows: () -> [Row<T, R>]
 ) {
     ParameterizedTest(target: targetFunction, customAssertion: customAssertion).execute(with: rows())
-}
-
-@available(swift 5.1)
-public func expect<T, R>(
-    _ row: ExpectRow<T, R>,
-    file: StaticString = #file,
-    line: UInt = #line
-) -> Row<T, R> {
-    Row(args: row.args, expect: row.expect, file: file, line: line)
 }
