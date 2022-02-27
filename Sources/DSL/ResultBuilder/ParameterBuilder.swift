d8c632e04a1c56c06ca4a21ce16eb5d1a871cd9c
sahara-ooga <26447279+sahara-ooga@users.noreply.github.com>
2022-02-27 15:48:02 +0900

    Revive implementation for swift5.1 ~ 5.3

--- Sources/DSL/ResultBuilder/ParameterBuilder.swift
+++ Sources/DSL/ResultBuilder/ParameterBuilder.swift
@@ -5,7 +5,7 @@
 //  Created by Yusuke Hosonuma on 2020/03/21.
 //
 
-@available(swift 5.4)
+#if compiler(>=5.4)
 @resultBuilder
 public struct ParameterBuilder1<T1, R> where R: Equatable {
     public typealias Row = Row1<T1, R>
@@ -54,3 +54,50 @@ public struct ParameterBuilder5<T1, T2, T3, T4, T5, R> where R: Equatable {
         rows
     }
 }
+
+#elseif compiler(>=5.1)
+@_functionBuilder
+public struct ParameterBuilder1<T1, R> where R: Equatable {
+    public typealias Row = Row1<T1, R>
+
+    public static func buildBlock(_ rows: Row...) -> [Row] {
+        rows
+    }
+}
+
+@_functionBuilder
+public struct ParameterBuilder2<T1, T2, R> where R: Equatable {
+    public typealias Row = Row2<T1, T2, R>
+
+    public static func buildBlock(_ rows: Row...) -> [Row] {
+        rows
+    }
+}
+
+@_functionBuilder
+public struct ParameterBuilder3<T1, T2, T3, R> where R: Equatable {
+    public typealias Row = Row3<T1, T2, T3, R>
+
+    public static func buildBlock(_ rows: Row...) -> [Row] {
+        rows
+    }
+}
+
+@_functionBuilder
+public struct ParameterBuilder4<T1, T2, T3, T4, R> where R: Equatable {
+    public typealias Row = Row4<T1, T2, T3, T4, R>
+
+    public static func buildBlock(_ rows: Row...) -> [Row] {
+        rows
+    }
+}
+
+@_functionBuilder
+public struct ParameterBuilder5<T1, T2, T3, T4, T5, R> where R: Equatable {
+    public typealias Row = Row5<T1, T2, T3, T4, T5, R>
+
+    public static func buildBlock(_ rows: Row...) -> [Row] {
+        rows
+    }
+}
+#endif
